2fc1acd8696747571fa72cdeb17d37a61d9f5c8f
Andreas Gullberg Larsen <anjdreas@gmail.com>
2015-07-24 00:08:22 +0200

    typedef: Add run() and make options arg optional

--- anjdreas-nested-yargs.d.ts
+++ anjdreas-nested-yargs.d.ts
@@ -4,31 +4,28 @@
 // Definitions: NOT UPLOADED YET
 // https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path="../yargs/yargs.d.ts" />
-
 declare module "anjdreas-nested-yargs" {
     import Yargs = require('yargs');
-//    module NestedYargs {
 
-        export interface Command {
-            name: string;
-            description: string;
-            options: any;
-            parent: Category;
-            path: string;
+    export interface Command {
+        name: string;
+        description: string;
+        options: any;
+        parent: Category;
+        path: string;
 
-            run: (yargs: Yargs.Argv) => {};
-        }
+        run: (yargs: Yargs.Argv) => {};
+    }
 
-        export interface Category extends Command {
-            commands: any[];
-            command: (cmd: Command) => {};
-        }
+    export interface Category extends Command {
+        commands: any[];
+        command: (cmd: Command) => {};
+    }
 
-        export function createApp(options: any): Category;
+    export function createApp(options?: any): Category;
+    export function run(app: Category, yargs: Yargs.Argv): Category;
 
-        export function createCategory(name: string, description: string, options?: any): Category;
+    export function createCategory(name: string, description: string, options?: any): Category;
 
-        export function createCommand(name: string, description: string, options?: any): Command;
-//    }
+    export function createCommand(name: string, description: string, options?: any): Command;
 }
